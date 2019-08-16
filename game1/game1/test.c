eb7643d72451be563c736de04422a56e31cb2b2a
Lemonsj <1480845939@qq.com>
2019-08-16 22:39:36 +0800

    日常练习

---
+++ game1/game1/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"game.h"
+
+void menu()
+{
+	printf("**************************\n");
+	printf("*****    1.Play      *****\n");
+	printf("*****    0.Exit      *****\n");
+	printf("**************************\n");
+}
+
+void game()
+{
+	char board[ROW][COL] = { 0 };
+	char ret = 0;
+	
+	printf("玩游戏!\n");
+	InitBoard(board, ROW, COL);
+	DisplayBoard(board, ROW, COL);
+	while (1)
+	{
+		
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = isWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = isWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret ==  '*')
+	{
+		printf("玩家赢!\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢!\n");
+	}
+	else if (ret == 'P')
+	{
+		printf("平局!\n");
+	}
+
+}
+void test()
+{
+	int input = 0;
+	//srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏!\n");
+			break;
+		default:
+			printf("选择错误!\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
