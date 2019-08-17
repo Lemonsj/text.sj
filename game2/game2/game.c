dfef62f2c1a3055f94f45ed9994119b64adf973d
Lemonsj <1480845939@qq.com>
2019-08-17 15:46:12 +0800

    日常练习

---
+++ game2/game2/game.c
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"game.h"
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < cols; j++)
+		{
+			board[i][j] = set;
+		}
+	}
+}
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i <= row; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	for (i = 1; i <= row; i++)
+	{
+		printf("%d ", i);
+		for (j = 1; j <= col; j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void SetMine(char mine[ROWS][COLS], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int count = Lcount;
+	
+	while (count>0)
+	{
+		x = rand() % row + 1;
+		y = rand() % col + 1;
+		if (mine[x][y] == '0')
+		{
+			mine[x][y] = '1';
+			count--;
+		}
+	}
+}
+int GetMine(char mine[ROWS][COLS], int x, int y)
+{
+	return mine[x - 1][y] +
+		mine[x - 1][y - 1] +
+		mine[x][y - 1] +
+		mine[x + 1][y - 1] +
+		mine[x + 1][y] +
+		mine[x + 1][y + 1] +
+		mine[x][y + 1] +
+		mine[x - 1][y + 1] - (8*'0');
+
+}
+
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	int count = 0;
+	while (row*col-Lcount-count>0)
+	{
+		printf("请输入坐标:>");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (mine[x][y] == '1')
+			{
+				printf("你被炸死了!\n");
+				DisplayBoard(mine, row, col);
+				break;
+			}
+			else
+			{
+				ret = GetMine(mine, x, y);
+				show[x][y] = ret + '0';
+				DisplayBoard(show, row, col);
+				count++;
+			}
+		}
+		else
+		{
+			printf("输入坐标错误，请重新输入:>");
+		}
+	}
+	if (row*col - Lcount - count == 0)
+	{
+		printf("恭喜你，排雷成功!\n");
+		DisplayBoard(mine, row, col);
+	}
+
+
+}
+
+
+
+
+
+
+
