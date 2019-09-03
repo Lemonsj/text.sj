fb6653321de3e018d094f39d6beb28743f36d2fe
Lemonsj <1480845939@qq.com>
2019-09-03 18:28:52 +0800

    日常练习

---
+++ test_文件/test_文件/test.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//struct S
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	printf("%p\n", &s);
+//	printf("%p\n", &(s.c1));
+//	printf("%p\n", &(s.i));
+//	printf("%p\n", &(s.c2));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1000; 
+//	FILE* pf = fopen("test.txt", "wb");
+//	fwrite(&a, 4, 1, pf);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int ch = getchar();
+//	printf("%c\n", ch);
+//	ch = getchar();
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+#include<errno.h>
+#include<strng.h>
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	//写文件
+//
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
