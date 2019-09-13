63935f7823c1a24aabe2d2b5098085521d34cb11
Lemonsj <1480845939@qq.com>
2019-09-13 11:53:41 +0800

    日常练习

---
+++ test_9_13/test_9_13/test.c
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//#define ROW 4
+//#define COL 4
+//
+//int search_num(int str[ROW][COL], int k)
+//{
+//	int x = 0;
+//	int y = COL - 1;
+//	while (x < ROW && y >= 0)
+//	{
+//		if (str[x][y] == k)
+//		{
+//			return 1;
+//		}
+//		else if (str[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else
+//		{
+//			x++;
+//		}
+//
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int k = 0;
+//	int arr[ROW][COL] = { { 1, 2, 8, 9 }, { 2, 4, 9, 12 }, { 4, 7, 10, 13 }, { 6, 8, 11, 15 } };
+//	int ret = 0;
+//	scanf("%d", &k);
+//	ret = search_num(arr, k);
+//	if (ret == 1)
+//	{
+//		printf("找到了\n");
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char str[10];
+//	str = "string";
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		i = i + 1;
+//		if (i == 5)
+//		{
+//			continue;
+//		}
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		if (i < 1)
+//		{
+//			continue;
+//		}
+//		if (i == 5)
+//		{
+//			break;
+//		}
+//		i++;
+//	}
+//	printf("%d\n", i);
+//	return 0;
+//}
+
+//#include<string.h>
+//#include<assert.h>
+//
+//char* replace(char* str, int k)
+//{
+//	assert(str != NULL);
+//	char* dest = str;
+//
+//}
+//
+//int main()
+//{
+//	char arr[] = "abc def xyz";
+//	int sz = strlen(arr);
+//	replace(arr, sz);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
