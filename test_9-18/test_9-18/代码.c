d54483a70eef56ae7c7ac1ddd49f45e8fd7e232c
Lemonsj <1480845939@qq.com>
2019-09-21 18:09:57 +0800

    日常练习

---
+++ test_9-18/test_9-18/代码.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int x = 0;
+//	x = 5 > 1 + 2 && 2 || 2 * 4 < 4 - !0;
+//	printf("%d\n", x);
+//	return 0;
+//}
+
+//int fun(int x, int y)
+//{
+//	static int m = 0;
+//	static int i = 2;
+//	i += m + 1;
+//	m = i + x + y;
+//	return m;
+//}
+//int main()
+//{
+//	int j = 4;
+//	int m = 1;
+//	int k;
+//	k = fun(j, m);
+//	printf("%d,", k);
+//	k = fun(j, m);
+//	printf("%d,", k);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	int *pa;
+//	pa = a;
+//	printf("%d", *pa);
+//	return 0;
+//}
+
+
+//int Change(int *px)
+//{
+//	int y = 8;
+//	y = y - *px;
+//	px = &y;
+//	return 0;
+//}
+//int main()
+//{
+//	int xx = 3;
+//	int *py = &xx;
+//	Change(py);
+//	printf("%d\n", *py);
+//	return 0;
+//}
