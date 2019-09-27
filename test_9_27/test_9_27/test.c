17989fbdf8ac52ebc8bc36bac43559a03892f8a9
Lemonsj <1480845939@qq.com>
2019-09-27 23:02:20 +0800

    日常练习

---
+++ test_9_27/test_9_27/test.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//#define ROW 3
+//#define COL 3
+//
+//int search_num(int str[ROW][COL], int k)
+//{
+//	int x = 0;
+//	int y = COL - 1;
+//	while (x < ROW - 1 && y >= 0)
+//	{
+//		if (str[x][y] = k)
+//		{
+//			return 1;
+//		}
+//		else if (str[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			y--;
+//		}
+//	}
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int arr[ROW][COL] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+//	int k = 6;
+//	int ret = 0;
+//	ret = search_num(arr,k);
+//	if (ret == 1)
+//	{
+//		printf("找到了\n");
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//
+//}
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	if ((++i > 0) || (++j > 0)){
+//		printf("i = %d, j = %d\n", i, j);
+//	}
+//	return 0;
+//}
+
+//int x = 0;
+//switch (++x)
+//{
+//case 0:++x;
+//case 1:++x;
+//case 2:++x;
+//}
