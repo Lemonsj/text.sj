2cf06038e49c38457aae6169270592e64686fecb
Lemonsj <1480845939@qq.com>
2019-09-30 07:29:31 +0800

    日常练习

---
+++ test_9_30/test_9_30/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+//void func(char* str)
+//{
+//	printf("%d\n", sizeof(str));
+//	printf("%d\n", strlen(str));
+//}
+//int main()
+//{
+//	char arr[] = "hello";
+//	func(arr);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch ;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch >= 'a' && ch <= 'z')
+//		{
+//			putchar(ch - 32);
+//		}
+//		else if (ch >= 'A' && ch <= 'Z')
+//		{
+//			putchar(ch + 32);
+//		}
+//		else if (ch >= '0' && ch <= '0')
+//		{
+//			;
+//		}
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char input[10] = { 0 };
+//	system("shutdown -s -t 60");
+//again:
+//	printf("电脑将在一分钟后关机，如果输入：王晨艺，就取消关机！\n");
+//	scanf("%s", input);
+//	if (strcmp(input, "王晨艺") == 0)
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
