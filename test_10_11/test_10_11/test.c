e75bc97438d7ac6dd042d9453b612b8a6bf06e3f
Lemonsj <1480845939@qq.com>
2019-10-11 23:31:37 +0800

    日常练习

---
+++ test_10_11/test_10_11/test.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+
+//int main()
+//{
+//	int a = 10;
+//	a = a++;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int sum = 0;
+//	int q = 1;
+//	int i = 0; 
+//	int j = 0;
+//	for (i = 1; i < 5; i++)
+//	{
+//		q = 1;
+//		for (i = 1; j < i; j++)
+//		{
+//			q *= i;
+//		}
+//		sum += q;
+//	}
+//
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	int n = 1;
+//	int sum = 0;
+//	int i = 0;
+//	for (i = 1; i <= 5; i++)
+//	{
+//		n *= i;
+//		sum += n;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
