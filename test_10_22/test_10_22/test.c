4dcf337829d37a248d86d279616940f8b8f9735f
Lemonsj <1480845939@qq.com>
2019-10-22 23:35:34 +0800

    日常练习

---
+++ test_10_22/test_10_22/test.c
@@ -0,0 +1,74 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//public class Test {
+//
+//	public static int sum(int num) {
+//		if (num == 1) {
+//			return 1;
+//		}
+//		else {
+//			return num + sum(n - 1);
+//		}
+//
+//		public static void main(String[] args) {
+//			int num = 10;
+//			int ret = 0;
+//			ret = sum(num);
+//			System.out.println(ret);
+//		}
+//	}
+//}
+//
+//public class Test {
+//
+//	public static int sum(int num) {
+//		if (num == 1) {
+//			return 1;
+//		}
+//		else {
+//			return num + sum(n - 1);
+//		}
+//
+//		public static void main1(String[] args) {
+//			int num = 10;
+//			int ret = 0;
+//			ret = sum(num);
+//			System.out.println(ret);
+//		}
+//		public ststic int func(int x) {
+//			if (x<9)
+//			{
+//				return x;
+//			}
+//			else {
+//
+//				return x % 10 + func(x / 10);
+//			}
+//			public static void main2(String[] args) {
+//				int num = 1729;
+//				int ret = 0;
+//				ret = func(num);
+//				System.out.println(ret);
+//			}
+//			public static int func1(int x) {
+//				if (x == 1)
+//				{
+//					return 1;
+//				}
+//				if (x == 2)
+//				{
+//					return 2;
+//				}
+//				else {
+//					return func(n - 1) + func(n - 2);
+//				}
+//			}
+//
+//			public static void main(String[] args) {
+//				int num = 4;
+//				int ret = 0;
+//				ret = func1(num);
+//				System.out.println(ret);
+//			}
+//		}
+//
