c2e1d11dd178fdee525185e71703cd3680e94f38
Lemonsj <1480845939@qq.com>
2019-10-27 23:39:50 +0800

    日常练习

---
+++ test_10_27/test_10_27/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+	}
+	//    public static void main1(String[] args) {
+	//        //int[] array = {1,4,2,5,3,6};
+	//        //int[] array = {1,2,3,4,5,6};
+	//        int max = 0;
+	//        int min = 0;
+	//        int ret = 0;
+	//        double aver = 0.0;
+	//        int key = 6;
+	//        int left = 0;
+	//       // int right = array.length-1;
+	//
+	//        int[] array = {1,2,3,4,5,6};
+	//        for(int i = 0; i<array.length-1; i++) {
+	//            System.out.println(array[i]);
+	//        }
+	//
+	//        //System.out.println(Arrays.toString(array));
+	//        max = num_max(array);
+	//        min = num_min(array);
+	//        aver = array_aver(array);
+	//        //reverse_array(array);
+	//        ret = binarySearch(array, key, left, right );
+	//        bubbleSort(array);
+
+
+	//        System.out.println(max);
+	//        System.out.println(min);
+	//        System.out.println(aver);
+	//        //System.out.println(Arrays.toString(array));
+	//       // System.out.println(ret);
+	//        System.out.println(Arrays.toString(array));
+
+	//   }
