ad7298d9b4976569c78ae0016e4b9f28062a0da0
Lemonsj <1480845939@qq.com>
2019-11-13 23:27:05 +0800

    日常练习

---
+++ 20191113/src/MySingleList.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode cur = head;
+        ListNode prev = null;
+        ListNode newHead = null;
+        while(cur != null) {
+            ListNode curNext = cur.next;
+            if(curNext == null) {
+                newHead = cur;
+            }
+            cur.next = prev;
+            prev = cur;
+            cur = cur.next;
+        }
+        return newHead;
+    }
+
+};
