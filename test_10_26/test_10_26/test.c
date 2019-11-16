70d5cd60f2daf4e44f1c80c1f50a4f5c86ab97ab
Lemonsj <1480845939@qq.com>
2019-11-16 23:56:47 +0800

    日常练习

--- test_10_26/test_10_26/test.c
+++ test_10_26/test_10_26/test.c
@@ -1,4 +1,4 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 //
 //import java.util.Arrays;
 //
@@ -107,3 +107,49 @@
 //		myArrayList.display();
 //	}
 //}
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	//int a = 0; 
+	//int b = 0;
+	//int c = 0;
+	//int d = 0;
+	///*for (a = 0; a < 2; a++) {
+	//	
+	//}*/
+	//for (b = 0; b < 2; b++) {
+	//		for (c = 0; c < 2; c++) {
+	//			for (d = 0; d < 2; d++) {
+	//				if (b != 1 && c == 1 && c != 1 && d != 1)
+	//				{
+	//					printf("小冰说谎\n");
+	//				}
+	//				if (b == 1 && c != 1 && c != 1 && d != 1)
+	//				{
+	//					printf("小火说谎\n");
+	//				}
+	//				if (b == 1 && c == 1 && c == 1 && d != 1)
+	//				{
+	//					printf("小木说谎\n");
+	//				}
+	//				if (b == 1 && c == 1 && c != 1 && d == 1)
+	//				{
+	//					printf("小水说谎\n");
+	//				}
+	//			}
+	//		}
+	//	}
+
+	//int a = 0;
+	//scanf("%d", &a);
+	////printf("%d\n", (a&(a - 1)) == 0);
+	//while (a > 1)
+	//{
+	//	a /= 2;
+	//}
+	//printf("%d\n", a);
+	printf("%x\n", -1);
+	system("pause");
+	return 0;
+}
